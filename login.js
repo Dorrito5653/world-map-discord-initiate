9f805e60ccb262ac059cc9610b8bebda6b738907
Dorrito5653 <dorritoshah1543@gmail.com>
2022-09-09 18:21:34 -0400

    yes

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
-var username = document.getElementById("username");
-var password = document.getElementById("password").value;
-console.log(username, password)
+const { LoginSchemaModel } = require('./dbconfig')
+async function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username)
+    console.log(password)
+    const log = await LoginSchemaModel.find({ username: username, password: password})
+    console.log('--------')
+    if (!log) return document.writeln('could not find that login, please try again')
+}
