83bd471de281a6227cb8a93500ba51e27cc2c56c
Dorrito5653 <dorritoshah1543@gmail.com>
2022-09-13 18:53:21 -0400

    added post request to route.rest

--- tools/routes/config.js
+++ tools/routes/config.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router()
-const accounts = require('../models/login')
+const Account = require('../models/login')
+
 
 //Get all
 router.get('/', async (req, res) => {
     try {
-        const account = accounts.find({})
-        res.json(account)
+        const accounts = await Account.find()
+        res.json(accounts)
     } catch (err){
         res.status(500).json({ message: err.message })
     }
-    res.send('Hello World')
 })
 
 //Get one
@@ -18,8 +18,18 @@ router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
 //Creating one
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    const acc = new Account({
+        username: req.body.username,
+        password: req.body.password,
+        VIP: req.body.VIP,
+    })
+    try {
+        const newacc = await acc.save()
+        res.status(201).json(newacc)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 //Updating One
 router.patch('/:id', (req, res) => {
