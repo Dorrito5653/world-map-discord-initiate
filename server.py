e44f1566126f033ddb6667da8dd13b1564b141a9
username121546434 <gokuleatharv06@gmail.com>
2022-09-15 20:02:36 -0400

    add logging to python server

--- server.py
+++ server.py
@@ -5,6 +5,20 @@ import socket
 import threading
 import json
 import traceback
+import logging
+import os
+
+if not os.path.exists('./logs'):
+    os.mkdir('logs')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s | %(threadName)s]: %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(f"./logs/log.log", encoding='utf-8')
+    ]
+)
 
 server = socket.gethostbyname(socket.gethostname())
 port = 5555
@@ -12,11 +26,11 @@ port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind((server, port))
-print(f'Socket binded successfully as {server, port}')
+logging.info(f'Socket binded successfully as {server, port}')
 
 
 s.listen()
-print('Waiting for connection...')
+logging.info('Waiting for connection...')
 
 
 def new_client(conn: socket.socket):
@@ -26,11 +40,15 @@ def new_client(conn: socket.socket):
             data = json.loads(conn.recv(2048).decode())
             conn.send(reply.encode())
 
+            # Don't log the sent and recieved to prevent log being large
             print(f'Recieved: {data}')
             print(f'Sending: {reply}')
         except Exception:
-            print("Unkown Error, disconecting imeidietly")
-            print(traceback.format_exc())
+            logging.info("Unkown Error, disconecting imeidietly")
+            logging.info(traceback.format_exc())
+            break
+    
+    conn.close()
 
 
 while True:
