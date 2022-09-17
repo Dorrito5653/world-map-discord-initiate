66e8d7a756ed418df25a7bd385eefc027e60d87b
Dorrito5653 <dorritoshah1543@gmail.com>
2022-09-17 17:35:26 -0400

    made some changes

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,39 +1,25 @@
-async function getLogin(username, password){
+function getLogin(){
+  let usernameElement = document.querySelector('#username')
+  let passwordElement = document.querySelector('#password')
+
   const request = new XMLHttpRequest()
   request.responseType = "json";
-  request.open("GET", `http://localhost:3000/config/${username}`, true);
+  const url = `http://localhost:3000/config/${usernameElement.value}`
+  request.open("GET", url, true);
   let jsonResponse;
-  request.onload = function(){
-    jsonResponse = request.response;
-  };
-  return jsonResponse;
+  request.onload(
+    function(){
+      jsonResponse = request.response;
+    }
+  );
+  return {jsonResponse, passwordElement};
 }
 
 
-async function submitLoginInfo () {
-    let usernameElement = document.querySelector('#username')
-    let passwordElement = document.querySelector('#password')
-
-    // let response = await fetch(`http://localhost:3000/config/${usernameElement.value}`)
-    // let data = await response.json()
-
-    console.log(`Username Element is HTMLElement: ${usernameElement instanceof HTMLElement}`);
-    console.log(`Username Element is HTMLElement: ${passwordElement instanceof HTMLElement}`);
-
-    alert(`username = ${usernameElement.value}`);
-    alert(`password = ${passwordElement.value}`);
-
-    const login = await getLogin(usernameElement.value, passwordElement.value);
-    alert(login)
-    // alert('hello');
-    // alert('str')
-    // if (!data) return 'Cannot find user'
-    // return data;
-      // .then(console.log(usernameElement.value))
-    
-
-
-    // if (!request.response) alert(`No such login found`);
-    // else return { src: '../start.html'};
-  }
+function validateLogin (json, password) {
+  const username = json.username;
+  const actualpassword = json.password;
+  if (password == actualpassword) return true
+  else return false;
+}
   
