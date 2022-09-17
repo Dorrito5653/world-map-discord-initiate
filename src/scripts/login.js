9fc75a356174f236a0eb9fdf39c1a43fea005cb3
Dorrito5653 <dorritoshah1543@gmail.com>
2022-09-16 21:44:34 -0400

    hi

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,30 +1,34 @@
+async function getLogin(username, password){
+  const request = new XMLHttpRequest()
+  request.open("GET", `http://localhost:3000/config/${username}`);
+  request.send();
+  request.onload(console.log(request));
+  return request.response;
+}
+
+
 async function submitLoginInfo () {
     let usernameElement = document.querySelector('#username')
     let passwordElement = document.querySelector('#password')
 
-    let response = await fetch(`http://localhost:3000/config/${usernameElement.value}`)
-    let data = await response.json()
+    // let response = await fetch(`http://localhost:3000/config/${usernameElement.value}`)
+    // let data = await response.json()
 
     console.log(`Username Element is HTMLElement: ${usernameElement instanceof HTMLElement}`);
     console.log(`Username Element is HTMLElement: ${passwordElement instanceof HTMLElement}`);
 
-    // alert(`usernameElement = ${usernameElement}`);
-    // alert(`passwordElement = ${passwordElement}`);
-
     alert(`username = ${usernameElement.value}`);
     alert(`password = ${passwordElement.value}`);
 
-    alert('hello');
-    alert('str')
+    const login = await getLogin(usernameElement.value, passwordElement.value);
+    alert(login)
+    // alert('hello');
+    // alert('str')
     // if (!data) return 'Cannot find user'
     // return data;
       // .then(console.log(usernameElement.value))
-
     
-    // const request = new XMLHttpRequest()
-    // request.open("GET", `http://localhost:3000/config/${usernameElement.value}`);
-    // request.send();
-    // console.log(request.response)
+
 
     // if (!request.response) alert(`No such login found`);
     // else return { src: '../start.html'};
