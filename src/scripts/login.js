46b6f39f0e1c414027672e7093de8199d2b871ef
Dorrito5653 <dorrito1543@gmail.com>
2022-09-25 18:12:13 -0400

    yes

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,24 +1,26 @@
 async function getLogin(){
   let usernameElement = document.querySelector('#username')
   let passwordElement = document.querySelector('#password')
-  let newWindow = window.open(
-    `http://localhost:3000/config/${usernameElement.value}`,
-    '_blank'
-  )
-  
-  const json = newWindow.document.getElementsByTagName('pre')[0].innerHTML
-  refreshedwindow.alert(json)
-  // newWindow.close()
-  // try {
-  //   const request = new XMLHttpRequest()
-  //   request.open("GET", url, true);
-  //   let jsonResponse;
-  //   request.send()
-  //   request.onload = function(){
-  //     jsonResponse = request.response
-  //     alert(request.response)
-  //   };
-  // } catch(err){
-  //   alert(err)
-  // }
+  // window.open(
+  //   `https://dorrito5653.github.io/world-map-discord-initiate/src/index.html`,
+  //   '_blank'
+  // )
+  // console.log('------')
+  if (usernameElement.value == '' | passwordElement.value == '') {
+    alert('Error, no username/password given');
+    return;
+  }
+  const url = `http://localhost:3000/config/${usernameElement.value}`
+  try {
+    const request = new XMLHttpRequest()
+    request.open("GET", url, true);
+    let jsonResponse;
+    request.send()
+    request.onload = function(){
+      jsonResponse = request.response
+      alert(request.response)
+    };
+  } catch(err){
+    alert(err)
+  }
 }
