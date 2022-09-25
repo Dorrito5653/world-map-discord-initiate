6f65f93a2cbb82e836448e6c5d18f513f12521ef
Dorrito5653 <dorrito1543@gmail.com>
2022-09-25 17:42:45 -0400

    also yes

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,23 +1,24 @@
 async function getLogin(){
-  console.log('------')
   let usernameElement = document.querySelector('#username')
   let passwordElement = document.querySelector('#password')
-
-  if (usernameElement.value == '' | passwordElement.value == '') {
-    alert('Error, no username/password given');
-    return;
-  }
-  const url = `http://localhost:3000/config/${usernameElement.value}`
-  try {
-    const request = new XMLHttpRequest()
-    request.open("GET", url, true);
-    let jsonResponse;
-    request.send()
-    request.onload = function(){
-      jsonResponse = request.response
-      alert(request.response)
-    };
-  } catch(err){
-    alert(err)
-  }
+  let newWindow = window.open(
+    `http://localhost:3000/config/${usernameElement.value}`,
+    '_blank'
+  )
+  
+  const json = newWindow.document.getElementsByTagName('pre')[0].innerHTML
+  refreshedwindow.alert(json)
+  // newWindow.close()
+  // try {
+  //   const request = new XMLHttpRequest()
+  //   request.open("GET", url, true);
+  //   let jsonResponse;
+  //   request.send()
+  //   request.onload = function(){
+  //     jsonResponse = request.response
+  //     alert(request.response)
+  //   };
+  // } catch(err){
+  //   alert(err)
+  // }
 }
