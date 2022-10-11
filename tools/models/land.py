7a313581fc1f66cdf5cd19508859ce54769a7492
username121546434 <gokuleatharv06@gmail.com>
2022-10-11 06:55:50 -0400

    added land dataclass

--- tools/models/land.py
+++ tools/models/land.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from dataclasses import dataclass
 
+if TYPE_CHECKING:
+    from .city import City
+    from .country import Country
+    from .country_province import CountryProvince
+    from .terrain import Terrain
 
+
+@dataclass
+class Land:
+    """Represents one km² of land"""
+    value: int # the money the land is worth in dollars
+    population: int
+    terrain: Terrain
+    city: City | None = None
+    country: Country | None = None
+    province: CountryProvince | None = None
